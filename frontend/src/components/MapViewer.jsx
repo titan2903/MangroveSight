6dac301a2afe0ac45d77ed31c6c4f2e322d40248
titan <titanio.yudista@cakrawala.ac.id>
2026-08-27 17:56:39 +0700

    refactor: replace CartoDB tile layers with Esri World Light and Dark Gray base maps

--- frontend/src/components/MapViewer.jsx
+++ frontend/src/components/MapViewer.jsx
@@ -198,8 +198,8 @@ const MapViewer = ({
         <LayersControl position="topleft">
           <BaseLayer checked name="🗺️ Light Map">
             <TileLayer
-              url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-              attribution='&copy; <a href="https://carto.com">CARTO</a>'
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+              attribution='&copy; <a href="https://www.esri.com/">Esri</a> &mdash; Esri, HERE, Garmin, NGA, USGS'
             />
           </BaseLayer>
           <BaseLayer name="🛰️ Satellite (Esri)">
@@ -210,8 +210,8 @@ const MapViewer = ({
           </BaseLayer>
           <BaseLayer name="🌙 Dark Mode">
             <TileLayer
-              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-              attribution='&copy; <a href="https://carto.com">CARTO</a>'
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+              attribution='&copy; <a href="https://www.esri.com/">Esri</a> &mdash; Esri, HERE, Garmin, NGA, USGS'
             />
           </BaseLayer>
           <BaseLayer name="🏔️ OpenTopo">
